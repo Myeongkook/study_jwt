1487aece87dbc144ed427977cbaab863eea63476
Myeongkook Park <myeongkook@vivans.net>
2022-04-01 12:43:27 +0900

    repo init

---
+++ src/main/java/com/jwt/benkyou/BenkyouApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.benkyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenkyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenkyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/benkyou/BenkyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.benkyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenkyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
